0e7b20a76a3defba31675788846354690d6cb298
RHZhang <zhangruihuai@gmail.com>
2017-10-17 17:06:41 +0800

    [Unstable] make RPi3+MATRIX Creator Streaming

--- src/MicMatrixCreator.cpp
+++ src/MicMatrixCreator.cpp
@@ -3,3 +3,97 @@
 //
 
 #include "MicMatrixCreator.h"
+
+extern Logger logger;
+
+MicMatrixCreator::MicMatrixCreator()
+    : Mic({kMicrophoneChannels, kSamplingRate, 16}, PlanarOrPacked::Planar, 256, nullptr)
+{
+
+}
+
+bool MicMatrixCreator::Start()
+{
+    // ref: mic_energy.cpp
+
+    matrix_hal::WishboneBus bus;
+    bus.SpiInit();
+
+    matrix_hal::MicrophoneArray mics;
+    m_nbSamplePerBlock=mics.NumberOfSamples();
+    mics.Setup(&bus);
+
+    matrix_hal::Everloop everloop;
+    everloop.Setup(&bus);
+
+    matrix_hal::EverloopImage image1d;
+    int j = 0;
+    uint64_t instantE = 0;
+    uint64_t avgEnergy = 0;
+    std::valarray<uint64_t> localAverage (20);
+    localAverage = 0;
+    mics.SetGain(8); // ?
+
+
+    if (!InitPA()){
+        return false;
+    }
+
+    for (;;){
+        mics.Read(); /* Reading 8-mics buffer from de FPGA */
+
+
+
+        err = Pa_WriteStream( m_stream, &(mics.Beam(0)), m_nbSamplePerBlock );
+        if( err ) {
+            logger("Pa_WriteStream error:", err);
+            break;
+        }
+
+    }
+
+
+
+    return true;
+}
+
+bool MicMatrixCreator::Stop() {
+    return false;
+}
+
+bool MicMatrixCreator::InitPA()
+{
+
+    try {
+        PaStreamParameters outputParameters;
+
+        outputParameters.device = Pa_GetDefaultOutputDevice(); /* default output device */
+        outputParameters.channelCount = 1;
+        outputParameters.sampleFormat = paInt16;
+        outputParameters.suggestedLatency = Pa_GetDeviceInfo(outputParameters.device)->defaultLowOutputLatency;
+        outputParameters.hostApiSpecificStreamInfo = nullptr;
+        /* -- setup stream -- */
+        auto err = Pa_OpenStream(
+                    &m_stream,
+                    nullptr,
+                    &outputParameters,
+                    kSamplingRate,
+                    m_nbSamplePerBlock,
+                    paClipOff,      /* we won't output out of range samples so don't bother clipping them */
+                    nullptr,
+                    this
+                    ); /* no callback, so no callback userData */
+
+        logger("Pa_OpenStream OK!");
+        if (err) {
+            logger("Pa_OpenStream failed:", err);
+            throw;
+        }
+    }
+    catch (exception &e) {
+        logger(e.what());
+    }
+
+
+    return paNoError == Pa_StartStream(m_stream);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,16 @@
 #include "Precompiled.h"
 #include "MicMatrixCreator.h"
 
-Logger gLogger("Mic.Array.Demo.log");
+Logger logger("Mic.Array.Demo.log");
 
 
 int main(void)
 {
-    gLogger("Main", "begin");
+    logger("Main", "begin");
 
-#ifdef RPI3_MATRIX_CREATOR
     MicMatrixCreator mc;
     mc.Start();
-#endif
 
-    gLogger("Main", "end");
+    logger("Main", "end");
     return 0;
 }
