b67d761182736126948c16713cc550161f93dbe2
RHZhang <zhangruihuai@gmail.com>
2017-10-18 15:29:50 +0800

    Add buidler.sh

---
+++ scripts/rk3229_linux/builder.sh
@@ -0,0 +1,2 @@
+# on Ubuntu 16.04 builder machine
+sudo apt install git gnupg flex bison gperf build-essential zip tar curl libc6-dev x11proto-core-dev  g++-multilib cmake tofrodos python-markdown libxml2-utils libncurses-dev -y
