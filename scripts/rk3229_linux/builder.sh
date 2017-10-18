585279e85bdda623cb6a392d988153805b15be3c
RHZhang <zhangruihuai@gmail.com>
2017-10-18 16:39:36 +0800

    Update builder.sh

--- scripts/rk3229_linux/builder.sh
+++ scripts/rk3229_linux/builder.sh
@@ -1,2 +1,2 @@
 # on Ubuntu 16.04 builder machine
-sudo apt install git gnupg flex bison gperf build-essential zip tar curl libc6-dev x11proto-core-dev lzop g++-multilib cmake tofrodos python-markdown libxml2-utils libncurses-dev -y
+sudo apt install git gnupg flex bison gperf build-essential zip tar curl libc6-dev x11proto-core-dev lzop g++-multilib cmake tofrodos python-markdown libxml2-utils openjdk-8-jdk libncurses-dev -y
